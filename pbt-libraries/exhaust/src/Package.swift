92f0fc3872aca84566e15f5621387d1e71ce75c2
Chris Kolbu <chris.kolbu@ittybittyapps.com>
2026-09-03 20:58:16 +1000

    Bump Exhaust version in Package.swift

--- pbt-libraries/exhaust/src/Package.swift
+++ pbt-libraries/exhaust/src/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS(.v15),
     ],
     dependencies: [
-        .package(url: "https://github.com/nesevis/exhaust", from: "0.1.0"),
+        .package(url: "https://github.com/nesevis/exhaust", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0")
     ],
     targets: [
